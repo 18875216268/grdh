2a30332cbff08aaac9a28a45ef4e2e3e4919b140
趣软铺 <117966904+18875216268@users.noreply.github.com>
2024-10-22 00:03:23 +0800

    Add files via upload

---
+++ ruanjianku/script-t.js
@@ -0,0 +1,17 @@
+
+window.onload = function () {
+    document.getElementById('弹窗容器id').style.display = 'flex'; // 页面加载时显示弹窗
+};
+
+function closeModal() {
+    window.close(); // 关闭窗口
+}
+
+function confirmAccess() {
+    document.getElementById('弹窗容器id').style.display = 'none'; // 隐藏弹窗
+}
+
+ // 返回按钮的点击事件
+ function goBack() {
+    window.location.href = '../index.html'; // 跳转到指定页面
+}
